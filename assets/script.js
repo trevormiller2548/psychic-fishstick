2423ca2210ed8048183d496f79d6775b56eb043f
trevormiller2548 <trevor.miller2548@gmail.com>
2023-05-18 21:26:01 -0600

    putting conditions on the rows changing colors

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,16 @@ $( document ).ready(function() {
 var a= dayjs().format('dddd MMMM Do YYYY, h:mm');
   $("#display-date").text(a)
 }
+
+   //This is so the rows change colors
+   function updateColor(){
+    var hour = new Date().getHours();
+    for (var i= 9 ; i<=18; i++){
+      console.log(hour,i)
+      if(hour==i ) {
+       $(`#inputText${i}`).css("background","lightblue")
+      }else  if(hour<i ){
+       
+        $(`#inputText${i}`).css("background","lightgreen")
+
+      }
